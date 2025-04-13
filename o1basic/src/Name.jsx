9ccf07ee848d0f187ad77c91ce234a03d485fc2c
roshanrjl <roshanrijal700@gmail.com>
2025-04-13 20:51:04 +0545

    costum react

---
+++ o1basic/src/Name.jsx
@@ -0,0 +1,8 @@
+
+function Names() {
+    return (
+      <h1>My name is roshan rijal</h1>
+    );
+  }
+  
+  export default Names;
---
+++ o1basic/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Names from './Name.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+  <Names />
+  <App />
+  </>
+  
+);
+
