e5d64f0be7c98c58f8c403454be039c3c9d94aa7
roshanrjl <roshanrijal700@gmail.com>
2025-04-29 15:31:46 +0545

    feat: enhance Appwrite services with CRUD operations for posts and file management

--- 12megaBlog/src/appwrite/configure.js
+++ 12megaBlog/src/appwrite/configure.js
@@ -3,7 +3,7 @@ import { Client, ID, Databases, Storage,Query } from "appwrite";
 
 export class Services{
     clinet = new Client()
-    Databases;
+    databases;
     Storage;
     
     constructor(){
@@ -15,6 +15,131 @@ export class Services{
         this.Databases = new Databases(this.clinet);
         this.Storage = new Storage(this.clinet);
     }
+
+
+    async createpost({title,slug, content, featuredImage, status,userId}){
+      try{
+        return await this.databases.createDocument(
+              conf.appwriteDatabase,
+            conf.appwriteCollection,
+            slug,
+            {
+              title,
+              content,
+              featuredImage,
+              status,
+               }
+        )
+
+      }catch(e){
+        console.log("Error creating post:", e);
+      }     
+
+    }
+
+    async updatepost(slug,{title, content, featuredImage, status,}){
+      try{
+        return await this.databases.updateDocument(
+          conf.appwriteDatabase,
+          conf.appwriteCollection,
+          slug,
+          {
+            title,
+            content,
+            featuredImage,
+            status,
+          }
+        )
+
+      }catch(e){
+        console.log("Error creating post:", e);
+      }
+
+}
+
+    async deletepost(slug){
+      try{
+        await this.databases.deleteDocument(
+          conf.appwriteDatabase,
+          conf.appwriteCollection,
+          slug
+        )
+        return true;
+        
+      }
+      catch(e){
+        console.log("Error creating post:", e);
+        return false;
+      }
+
+
+}
+
+    async getpost(slug){
+      try{
+        return await this.databases.getDocument(
+          conf.appwriteDatabase,
+          conf.appwriteCollection,
+          slug
+        )
+
+      }catch(e){
+        console.log("Error creating post:", e);
+        return false;
+      }
+    }
+
+    async getposts(queries = [Query.equal("status", "active")]){
+      try{
+        return await this.databases.listDocuments(
+          conf.appwriteDatabase,
+          conf.appwriteCollection,
+          queries
+        )
+      }catch(e){
+        console.log("Error creating post:", e);
+        return false;
+      }
+
+    }
+
+    async uploadfile(file){
+      try{
+         return await this .Storage.createFile(
+          conf.appwriteBUcketId,
+          ID.unique(),
+          file,
+         )
+
+      }catch(e){
+        console.log("Error creating post:", e);
+        return false;
+      }
+    }
+     //delete file
+    async deletefile(fileId){
+      try{
+        return await this.Storage.deleteFile(
+          conf.appwriteBUcketId,
+          fileId
+        )
+      }catch(e){
+        console.log("Error creating post:", e);
+        return false;
+      }
+    }
+     getfilepreview(fileId){
+      try{
+        return this.Storage.getFilePreview(
+          conf.appwriteBUcketId,
+          fileId,      
+        )
+      }catch(e){
+        console.log("Error creating post:", e);
+        return false;
+      }
+
+}
 
 }
 
